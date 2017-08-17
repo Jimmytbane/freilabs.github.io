c858ee9370db358d69841d30aee1f078d65080ad
jmfgdev <jmfgdev@outlook.com>
2017-08-17 05:36:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const { h, app } = hyperapp
 
 app({
   state: {
-    title: "Up and coming software..."
-    "pecan.py - Windows Security Incident Prevention Software</a> (repo currently empty, will be released when the program is not a disaster.)"
+    title: "pecan.py - Windows Security Incident Prevention Software</a> (repo currently empty, will be released when the program is not a disaster.)"
   },
   view: state => h("h1", {}, state.title)
 })
