57e16cf99f1d92b2ea67540f6a72b2065158715c
jmfgdev <jmfgdev@outlook.com>
2017-08-17 06:25:38 -0400

    try to fix hyperapp again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { h, app } = hyperapp
 
 app({
   state: {
-    title: "pecan.py - Windows Security Incident Prevention Software (repo currently empty, will be released when the program is not a disaster.)"
+    title: "pecan.py"
   },
   view: state => h("h1", {}, state.title)
 })
